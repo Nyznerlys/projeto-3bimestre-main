4402816e584f2c083c2af3eb3efff16072989c6e
sla <sla>
2025-08-14 12:02:20 -0300

    1

--- index.js
+++ index.js
@@ -3,12 +3,47 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+let usuarios = [];
+let contador = 1;
+
 app.use(express.json())
 
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
   res.json({message: "API Online"})
 })
+
+//ROTA GET Listar todos os itens
+app.get("/usuarios", (req, res) =>{
+  res.json({ message: "API Online"})
+})
+
+//ROTA POST Criar novo usuários
+app.post("/usuarios", (req, res) => {
+  const {nome} = req.body
+
+  const novoUsuario = {
+    id: contador++,
+    nome
+  };
+
+  usuarios.push(novoUsuario);
+  res.status(201).json(novoUsuario);
+});
+
+//ROTA PUT Atualiza usuario por ID
+app.put("/ususarios/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const {nome} = req.body;
+
+  const ususario = usuarios.find( (i) => i.id ===id);
+
+  if (!usuario) {
+    return res.status(404).json({erro: "Usuario não encontrado"});
+  }
+  usuario.nome = nome;
+  res.json(usuario);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
